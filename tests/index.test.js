bd5f19088e80724b8e5a41c687cf49f22f3919cd
Ivan Maksimenko <ivan.maksimenko@acropolium.com>
2014-08-05 14:36:06 +0300

    dev

--- tests/index.test.js
+++ tests/index.test.js
@@ -33,18 +33,35 @@ describe('Request-Reply', function() {
         stack.channels().length.should.be.eql(0);
     });
 
-    it('should add request handlers', function() {
-        stack.beforeReq();
-        stack.afterReq();
-        stack.onReq('read-file', function(elem, next) {
-            next.done = true;
-            async: true;
-            next(null)
-        }, {push: true, unshift: true});
+    it('should manage request handlers', function() {
+        var fn = function(fileName, next) {
+            next(null, 'file content');
+        };
+        var remove = stack.onReq('fs.readFile', fn);
+        should.exist(remove);
+        remove.should.be.type('function');
+        var match = stack.findMatch('fs.readFile');
+        match.should.be.an.Array.and.have.lengthOf(1);
+        match[0].fn.should.be.equal(fn);
+
+        remove();
+        stack.findMatch('fs.readFile').should.be.an.Array.and.have.lengthOf(0);
     });
 
-    it('should execute request', function() {
+    it('should execute request', function(done) {
+        var email = 'johndoe@mail.com';
 
+        stack.onReq('sendEmail', function(data, next) {
+            data.should.have.property('email', email);
+            console.log('Request call');
+            next(null, 'done ' + data.email);
+        });
+
+        stack.req('sendEmail', {email: email}, function(err, result) {
+            should.exist(result);
+            should.not.exist(err);
+            result.should.be.eql('done ' + email);
+            done();
+        });
     });
-
 });
