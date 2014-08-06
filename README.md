a8657c2e75a2da348b939b2420a78dcafd4468fe
Ivan Maksimenko <ivan.maksimenko@acropolium.com>
2014-08-06 14:21:56 +0300

    workman

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-req-repl
-========
+workman
+============
+
+## Installation
+    npm install workman 
+    
+## Example
+```javascript
+    var workman = require('workman'),
+        fs = require('fs');
+    
+    workman.define('fs.readFile', function(file, cb) {
+        fs.readFile(file, 'utf8', cb);
+    });
+    
+    workman.before('fs.readFile', function(file, cb) {
+        fs.exists(file, function(exists) {
+            if (!exists) {
+                return cb(new Error('File does not exist'));
+            }
+            cb(null, file);
+        });
+    });
+    
+    workman.after('fs.readFile', function(content, cb) {
+        console.log('After', content);
+        cb(null, content);
+    });
+    
+    workman.ns('fs').wrap('readFile', (function() {
+        var cache = {};
+        return function(file, fn, cb) {
+            if (cache.hasOwnProperty(file)) {
+                return cb(null, cache[file]);
+            }
+            fn(file, function(err, content) {
+                if (err) return cb(err);
+                cache[file] = content;
+                return cb(null, content);
+            });
+        };
+    }()));
+    
+    workman.ns('fs').do('readFile', __dirname + '/README.md', function(err, content) {
+        console.log(content);
+    }); //or invoke
+```
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,67 +1,132 @@
 var should = require('should'),
-    stack = require('../');
+    cmnds = require('../lib/index');
 
 /*global describe,before,it,after */
 
-describe('Request-Reply', function() {
-
+describe('Workman', function() {
     before(function() {
-        stack.should.be.an.Object;
+        cmnds.should.be.an.Object;
     });
 
-    it('should have global channel', function() {
-        ['remove', 'channel', 'onReq', 'offReq'].forEach(function(fn) {
-            stack.should.have.property(fn).with.type('function');
+    beforeEach(function() {
+        cmnds.reset();
+    });
+
+    describe('Global container', function() {
+        var methods = ['ns', 'delNs', 'containers', 'define', 'before', 'after', 'invoke', 'reset'];
+
+        beforeEach(function() {
+            cmnds.reset();
         });
 
-        stack.channel().should.be.equal(stack);
-    });
-
-    it('should manage channels', function() {
-        var name = 'chnl',
-            channel = stack.channel(name);
-
-        ['onReq', 'offReq'].forEach(function(fn) {
-            stack.should.have.property(fn).with.type('function');
+        it('should has methods "' +  methods.join(', ') + '"', function() {
+            methods.forEach(function(fn) {
+                cmnds.should.have.property(fn).with.type('function');
+            })
         });
-        stack.channel(name).should.be.equal(channel);
-        stack.channels().length.should.be.eql(1);
-        stack.names().should.be.eql([name]);
-        stack.has(name).should.be.true;
-        stack.remove(name);
-        stack.has(name).should.be.false;
-        stack.channels().length.should.be.eql(0);
+
+        it('should manage containers', function() {
+            var name = 'chnl',
+                container = cmnds.ns(name);
+
+            should(container).be.an.Object;
+            cmnds.ns(name).should.be.equal(container);
+            cmnds.namespaces().length.should.be.eql(1);
+            cmnds.containers().length.should.be.eql(1);
+            cmnds.hasNs(name).should.be.true;
+            cmnds.delNs(name);
+            cmnds.hasNs(name).should.be.false;
+            cmnds.namespaces().length.should.be.eql(0);
+        });
+
+        it('should manage cmnds', function() {
+            var fn = function(fileName, next) {
+                next(null, 'file content');
+            };
+            var remove = cmnds.define('fs.readFile', fn);
+            should.exist(remove);
+            remove.should.be.type('function');
+
+            cmnds.ns('fs').has('readFile').should.be.true;
+            cmnds.has('fs.readFile').should.be.true;
+            remove();
+            cmnds.has('fs.readFile').should.be.false;
+        });
     });
 
-    it('should manage request handlers', function() {
-        var fn = function(fileName, next) {
+
+    it('should invoke defined command', function(done) {
+        cmnds.ns('fs').define('readFile', function(file, next) {
+            should.exist(file);
+            file.should.be.eql('test.txt');
             next(null, 'file content');
-        };
-        var remove = stack.onReq('fs.readFile', fn);
-        should.exist(remove);
-        remove.should.be.type('function');
-        var match = stack.findMatch('fs.readFile');
-        match.should.be.an.Array.and.have.lengthOf(1);
-        match[0].fn.should.be.equal(fn);
-
-        remove();
-        stack.findMatch('fs.readFile').should.be.an.Array.and.have.lengthOf(0);
-    });
-
-    it('should execute request', function(done) {
-        var email = 'johndoe@mail.com';
-
-        stack.onReq('sendEmail', function(data, next) {
-            data.should.have.property('email', email);
-            console.log('Request call');
-            next(null, 'done ' + data.email);
         });
 
-        stack.req('sendEmail', {email: email}, function(err, result) {
-            should.exist(result);
+        cmnds.ns('fs').invoke('readFile', 'test.txt', function(err, content) {
             should.not.exist(err);
-            result.should.be.eql('done ' + email);
+            should.exist(content);
+            content.should.be.eql('file content');
             done();
         });
     });
+
+    it('should perform "before, after" functions', function(done) {
+        cmnds.define('fs.readFile', function(file, next) {
+            console.log('call hook');
+            should.exist(file);
+            file.should.be.eql('test#2.txt');
+            next(null, 'hello');
+        });
+
+        cmnds.before('fs.readFile', function(file, next) {
+            console.log('call before #1');
+            file.should.be.eql('test.txt');
+            next(null, 'test#1.txt');
+        });
+
+        cmnds.before('fs.readFile', function(file, next) {
+            console.log('call before #2');
+            next(null, 'test#2.txt');
+        });
+
+        cmnds.after('fs.readFile', function(content, next) {
+            console.log('call after #1');
+            next(null, content + ' master');
+        });
+
+        cmnds.after('fs.readFile', function(content, next) {
+            console.log('call after #2');
+            next(null, content + ' yoda');
+        });
+
+        cmnds.wrap('fs.readFile', (function() {
+            var cache;
+            return function(file, fn, next) {
+                console.log('call wrap');
+                if (!cache) {
+                    fn(file, function(err, content) {
+                        content.should.be.eql('hello');
+                        cache = content;
+                        next(null, cache);
+                    });
+                } else {
+                    next(null, 'from cache ' + cache);
+                }
+            }
+        }()));
+
+        cmnds.invoke('fs.readFile', 'test.txt', function(err, content) {
+            should.not.exist(err);
+            should.exist(content);
+            content.should.be.eql('hello master yoda');
+
+            cmnds.invoke('fs.readFile', 'test.txt', function(err, content) {
+                should.not.exist(err);
+                should.exist(content);
+                content.should.be.eql('from cache hello master yoda');
+                done();
+            });
+        });
+    });
+
 });
